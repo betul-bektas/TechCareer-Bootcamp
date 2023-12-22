704ac7d3ad64030ce42fd47e51340c4a51dd1f69
betulbektas1 <betul1279@hotmail.com>
2023-12-22 21:19:18 +0300

    Bootcamp-Ders-1

---
+++ WebApplication1/Controllers/CityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            string[] country = new string[] { "Türkiye", "Hindistan", "Almanya", "Abd", "Çin","Arjantin","Endonezya","Fransa","Güney Kore","Kuzey Kore", "Güney Afrika","İngiltere","Japonya","Kanada","Rusya","Meksika","Brezilya","Suudi Arabistan" };
+            return country;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string[] country = new string[] { "Türkiye", "Hindistan", "Almanya", "Abd", "Çin", "Arjantin", "Endonezya", "Fransa", "Güney Kore", "Kuzey Kore", "Güney Afrika", "İngiltere", "Japonya", "Kanada", "Rusya", "Meksika", "Brezilya", "Suudi Arabistan" };
+            string indeks = "";
+
+            // Check if the id is valid
+            if (id < 0 || id >= country.Length)
+            {
+                return "Invalid ID";
+            }
+            else
+            {
+                for (int i = 0; i < id; i++)
+                {
+                    indeks += country[i] + "\n";
+
+                }
+
+            }
+            return indeks;
+        }
+
+
+    }
+}
